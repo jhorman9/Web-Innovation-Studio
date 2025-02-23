3a2d680d40495ab7da6150133659b64da79ea317
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2025-02-22 23:56:52 -0500

    Se agrega favicon

--- includes/meta.php
+++ includes/meta.php
@@ -3,6 +3,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"> 
 
+<link rel="icon" href="./img/wis.png" type="image/x-icon">
+
 <!-- Icon Font Stylesheet -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
